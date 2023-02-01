2ccfd8b34a4aefd50e44608c4af008d5b77391e5
Pablo <pablogentili22@gmail.com>
2023-01-31 23:06:53 -0300

    Pokedex

---
+++ app.js
@@ -0,0 +1,95 @@
+const baseUrl = `https://pokeapi.co/api/v2/pokemon/`;
+const pokemon = document.getElementById('pokemonName');
+const buttonPokemon = document.getElementById('buscarPokemon');
+const buttonClear = document.getElementById('clearPokemon');
+const appNode = document.getElementById('app');
+
+buttonPokemon.addEventListener('click' , insertPokemon);
+buttonPokemon.addEventListener('touchstart' , insertPokemon); //* Para Celulares
+
+buttonClear.addEventListener('click' , deletePokemons);
+buttonClear.addEventListener('touchstart' , deletePokemons); //* Para celulares
+
+async function insertPokemon() {
+  try {
+    const res = await fetch(`${baseUrl}${pokemon.value.toLocaleLowerCase()}`)
+    const pokemonDataJSON = await res.json()
+
+    const allItems = [];
+    const result = []; //Guardo la respuesta
+
+    for (let pokemonInfo in pokemonDataJSON) { //* Paso Json a Array
+      result.push([pokemonInfo , pokemonDataJSON[pokemonInfo]]);
+    }
+
+    console.table(result); //Solo para ver la consola.
+
+    //*Crear imagen
+    const pokemonImage = document.createElement('img');
+    pokemonImage.src = result[14][1].front_default; // Imagen del pokemon
+
+    //*Nombre de pokemon e ID
+    const pokemonName = document.createElement('h2');
+    pokemonName.innerText = `Name: ${result[10][1]} - ID: ${result[6][1]}`; // Nombre pokemon con ID
+
+    //*Tipo de pokemon
+    const pokemonType = document.createElement('h2');
+    pokemonType.innerText = `Type: ${result[16][1][0].type.name}`; //Tipo de Pokemon
+
+    //* Pokemon HP
+    const hp = document.createElement('p');
+    hp.innerText = `HP: ${result[15][1][0].base_stat}`; // Vida de pokemon
+    hp.classList.add('pokemonStats');
+
+    //* Poder de ataque
+    const attack = document.createElement('p');
+    attack.innerText = `Attack: ${result[15][1][1].base_stat}`; // Ataque
+    attack.classList.add('pokemonStats');
+
+    //* Defensa
+    const defense = document.createElement('p');
+    defense.innerText = `Defense: ${result[15][1][2].base_stat}`; // Defensa
+    defense.classList.add('pokemonStats');
+
+    //* Ataque especial
+    const specialAttack = document.createElement('p');
+    specialAttack.innerText = `Special Attack: ${result[15][1][3].base_stat}`; //* Pokemon ataque especial
+    specialAttack.classList.add('pokemonStats');
+
+    // defensa especial
+    const specialDefense = document.createElement('p');
+    specialDefense.innerText = `Special Defense: ${result[15][1][4].base_stat}`; // defensa especial
+    specialDefense.classList.add('pokemonStats');
+
+    //* Velocidad
+    const speed = document.createElement('p');
+    speed.innerText = `Speed: ${result[15][1][5].base_stat}`; //* Velocidad
+    speed.classList.add('pokemonStats');
+
+    //* Estadisticas del contenedor
+    const stats = document.createElement('div');
+    stats.append(hp, attack, defense, specialAttack, specialDefense, speed);
+    stats.classList.add('pokemonStatsContainer');
+
+    //*Crear contenedor
+    const container = document.createElement('div');
+    container.append(pokemonImage , pokemonName ,pokemonType, stats);
+    container.classList.add('container');
+
+    allItems.push(container);
+
+    appNode.append(...allItems);
+
+  } catch (error) {
+    alert("Este pokemon NO ESTA DISPONIBLE, intenta con otro!");
+  }
+}
+
+function deletePokemons() {
+  let allPokemon = appNode.childNodes;
+  allPokemon = Array.from(allPokemon);
+
+  allPokemon.forEach(pokemon => {
+    pokemon.remove(pokemon);
+  });
+}
